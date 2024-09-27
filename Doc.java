d8186806cca7234ce6b62425ceb9e56a9279ae1f
Nguyen Thi Tu Quyen <130896745+nttq@users.noreply.github.com>
2024-09-27 11:10:35 +0700

    Update Doc.java

--- Doc.java
+++ Doc.java
@@ -13,19 +13,6 @@ public class Doc {
     public Doc(byte[] textContent) {
     }
 
-    //    public List<Word> getTitle() {
-//        List<Word> listTitle = new ArrayList<>();
-//        String[] list = this.content.split("\n")[0].split("\\s");
-//        listTitle = Arrays.stream(list).map(Word::createWord).collect(Collectors.toList());
-//        return listTitle;
-//    }
-//
-//    public List<Word> getBody() {
-//        List<Word> listBody = new ArrayList<>();
-//        String[] list = this.content.split("\n")[1].split("\\s");
-//        listBody = Arrays.stream(list).map(Word::createWord).collect(Collectors.toList());
-//        return listBody;
-//    }
     public List<Word> getTitle() {
         List<Word> wordList = new ArrayList<Word>();
         String[] line = this.content.split("\n");
@@ -60,14 +47,4 @@ public class Doc {
         }
     }
 
-//    public boolean equals(Object obj) {
-//        if( this == obj){
-//            return true;
-//        }
-//       if(!(obj instanceof Doc)){
-//               return false;
-//           }
-//       Doc doc = (Doc) obj;
-//       return this.getTitle().equals(doc.getTitle())&&this.getBody().equals(doc.getBody());
-//    }
 
